5d2e2e7d3015e1340051017d6c725aaf6f467154
Chaim Halbert <chaim.leib.halbert@gmail.com>
2022-05-25 23:00:50 -0700

    set header

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 import './sass/main.scss'
 import { hello } from './hello'
+import 'phaser/dist/phaser.min'
+
+document.head.innerHTML = `
+<meta charset="utf-8" />
+<title>Breakout</title>
+`
 
 document.body.innerHTML = `
 <div class="content">
